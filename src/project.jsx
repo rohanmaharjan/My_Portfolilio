343a0e5bd50700685b00759442d2846d5f056087
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-23 18:53:21 +0545

    added project.css

--- src/project.jsx
+++ src/project.jsx
@@ -2,17 +2,30 @@ import React from "react";
 import vegScan from "./assets/vegscan.png"
 import honeyPot from "./assets/Honey.png"
 import dictionary from "./assets/dictionary.png"
+import "./project.css"
 
 const Project = () => {
     return(
-        <div id="projects">
+        <div className="project-container" id="projects">
             <h1>My Projects</h1>
-            <img src={vegScan} alt="Vegscan" />
-            <p>Vegscan</p>
-            <img src={honeyPot} alt="Honeypot"  />
-            <p>HoneyPot System</p>
-            <img src={dictionary} alt="Dictionary" />
-            <p>English Dictionary</p>
+            <div className="project-grid">
+                <div className="project-item">
+                    <img src={vegScan} alt="Vegscan" />
+                    <p>Vegscan</p>
+                </div>
+                
+                <div className="project-item">
+                    <img src={honeyPot} alt="Honeypot"  />
+                    <p>HoneyPot System</p>
+                </div>
+                
+                <div className="project-item">
+                    <img src={dictionary} alt="Dictionary" />
+                    <p>English Dictionary</p>
+                </div>
+                
+            </div>
+                
         </div>
     )
 }
