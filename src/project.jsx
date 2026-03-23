cac7214280596456a2db26ff2ecbe55aea5d2e07
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-23 18:39:31 +0545

    added more projects

--- src/project.jsx
+++ src/project.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import vegScan from "./assets/vegscan.png"
+import honeyPot from "./assets/Honey.png"
+import dictionary from "./assets/dictionary.png"
 
 const Project = () => {
     return(
         <div id="projects">
             <h1>My Projects</h1>
-            <img src={vegScan} alt="Profile" className="profile-pic" />
+            <img src={vegScan} alt="Vegscan" />
             <p>Vegscan</p>
+            <img src={honeyPot} alt="Honeypot"  />
+            <p>HoneyPot System</p>
+            <img src={dictionary} alt="Dictionary" />
+            <p>English Dictionary</p>
         </div>
     )
 }
