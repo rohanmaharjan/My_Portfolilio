1bad44a57262a2ae431197da879eb6e6b7c41c5b
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-23 19:23:07 +0545

    added major, minor and ongoing projects

--- src/project.jsx
+++ src/project.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import vegScan from "./assets/vegscan.png"
 import honeyPot from "./assets/Honey.png"
 import dictionary from "./assets/dictionary.png"
+import gymPic from "./assets/gym.png"
 import "./project.css"
 
 const Project = () => {
     return(
         <div className="project-container" id="projects">
-            <h1>My Projects</h1>
+            <h1>Major Projects</h1>
             <div className="project-grid">
                 <div className="project-item">
                     <img src={vegScan} alt="Vegscan" />
@@ -23,8 +24,20 @@ const Project = () => {
                     <img src={dictionary} alt="Dictionary" />
                     <p>English Dictionary</p>
                 </div>
+
+                <div className="project-item">
+                    <img src={gymPic} alt="Gym" />
+                    <p>Gym Management System</p>
+                </div>
                 
             </div>
+            <h2>Minor Projects</h2>
+            <p>Student Managemnet System</p>
+            <p>Attendance Management System</p>
+            <p>Online Cycle Store</p>
+            <p>Online Quiz System</p>
+            <h2>Ongoing Project</h2>
+            <p>To-DO List</p>
                 
         </div>
     )
