1dfa1171d3114324e88036b196792e6d3ab4c026
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-31 18:58:11 +0545

    added achievements

--- src/achievements.jsx
+++ src/achievements.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './achievements.css'
 
 const Achievements = () => {
   return (
-    <div id='achievements'>
-      <h1>This is achievement page.</h1>
+    <div className='achievements-container' id='achievements'>
+      <h1>Achievements</h1>
+      
+        <ul>
+            <li>3 Days Python Django Workshop at Vrrit Technologies, 31st January, 2025</li>
+            <li>Participated in HULT Prize at Khwopa Engineering College, 2023</li>
+            <li>Participated in AWS Fundamentals Workshop by KhEC Knowledge Sharing Circle with AWS cloud club at TU, 16th August, 2025</li>
+            <li>Participated in Lumivya Technology's 1-Day AI and Data Bootcamp: ATLAS ( Agentic Tools, LLMs, and Scalable Systems), 29th March, 2026</li>
+        </ul>
+      
+
     </div>
   )
 }
