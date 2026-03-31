122263ef3ee45ce342a4fbf610d57d0e5c45906c
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-31 18:33:57 +0545

    achievement page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Navbar from "./navbar";
 import Home from "./home";
 import About from "./about";
 import Technologies from "./technologies";
-import Contact from "./contact"
+import Contact from "./contact";
 import Project from "./project";
-import Footer from "./footer"
+import Footer from "./footer";
+import Achievements from "./achievements";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <About />
       <Technologies />
       <Project />
+      <Achievements />
       <Contact />
       <Footer />
     </div>
---
+++ src/achievements.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Achievements = () => {
+  return (
+    <div id='achievements'>
+      <h1>This is achievement page.</h1>
+    </div>
+  )
+}
+
+export default Achievements
