399f30ac9cbe1d90154ca9daad0d72e30255eae7
Rohan Maharjan <rohanmhj9@gmail.com>
2026-04-05 18:45:47 +0545

    zoom in

--- src/achievements.jsx
+++ src/achievements.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './achievements.css';
 import hult from './assets/hult.jpg';
 import aws from './assets/aws.jpg';
 import lumivya from './assets/lumivya.png';
 
 const Achievements = () => {
+  const [ zoomedImages, setZoomedImages ] = useState(null);
+
   const certificate = [
     { src: hult, caption: "HULT PRIZE AT KhEC" },
     { src: aws, caption: "AWS FUNDAMENTALS WORKSHOP" },
@@ -16,15 +18,22 @@ const Achievements = () => {
       <h1>ACHIEVEMENTS</h1>
       <div className='achievements-grid'>
         {certificate.map((proj, index) => (
-          <div className='achievements-item'>
+          <div className='achievements-item' onClick={() => setZoomedImages(proj.src)}>
             <img src={proj.src} alt={proj.caption} />
             <hr />
             <p>{proj.caption}</p>
           </div>
         ))}
       </div>
+
+      {zoomedImages && (
+        <div className='overlay' onClick={() => setZoomedImages(null)}>
+          <img src={zoomedImages} alt="Zoomed" className='zoomed' />
+        </div>
+      )}
+      
     </div>
   )
 }
 
-export default Achievements
+export default Achievements;
