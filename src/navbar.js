800a1df0e4fd1bba810cf7b420e43f1fc6433f16
Rohan Maharjan <rohanmhj9@gmail.com>
2026-04-07 13:16:22 +0545

    hamburger icon added

--- src/navbar.js
+++ src/navbar.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 
 //component declaration
 const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return(
         <nav className="navbar">
             <div className="navbar-left">
                 <h1>My Portfolio</h1>
             </div>
 
-            <div className="navbar-right">
-                    <ul className="nav-links">
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#tech">Technology</a></li>
-                        <li><a href="#projects">Projects</a></li>
-                        <li><a href="#achievements">Achievements</a></li>
-                        <li><a href="#contact">Contact Me</a></li>
-                    </ul>
+            {/* hamburger icon */}
+            <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+                ☰
+            </div>
+
+            <div className={`navbar-right ${menuOpen ? "active" : ""}`}>
+                <ul className="nav-links">
+                    <li><a href="#home" onClick={() => setMenuOpen(false)}>Home</a></li>
+                    <li><a href="#about" onClick={() => setMenuOpen(false)}>About</a></li>
+                    <li><a href="#tech" onClick={() => setMenuOpen(false)}>Technology</a></li>
+                    <li><a href="#projects" onClick={() => setMenuOpen(false)}>Projects</a></li>
+                    <li><a href="#achievements" onClick={() => setMenuOpen(false)}>Achievements</a></li>
+                    <li><a href="#contact" onClick={() => setMenuOpen(false)}>Contact Me</a></li>
+                </ul>
             </div>
         </nav>
     );
